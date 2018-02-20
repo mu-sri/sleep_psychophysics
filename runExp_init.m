b0c2e8a6ab36d8bd49dba2b75107b33e0b3c6668
mu-sri <mu.sribanditmongkol@monash.edu>
2018-02-20 16:01:46 +1100

    Update runExp_init.m

--- runExp_init.m
+++ runExp_init.m
@@ -1,2 +1,2 @@
-PARTICIPANT_RESULT_SAVE_DIR='//Users/sleeping/Documents/MATLAB/ccshs_data/Result/'
+PARTICIPANT_RESULT_SAVE_DIR='/Users/sleeping/Documents/MATLAB/ccshs_data/Result/'
 IMAGE_DIR='/Users/sleeping/Documents/MATLAB/ccshs_data'
