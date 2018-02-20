590286e8d6aa237d503aa1869135b2a8d7064138
SleepingMAC <sleeping@mu00034079.mbi.monash.edu.au>
2018-02-20 16:03:51 +1100

    Typo

--- runExp_init.m
+++ runExp_init.m
@@ -1,2 +1,3 @@
-PARTICIPANT_RESULT_SAVE_DIR='//Users/sleeping/Documents/MATLAB/ccshs_data/Result/'
+PARTICIPANT_RESULT_SAVE_DIR='/Users/sleeping/Documents/MATLAB/ccshs_data/Result/'
 IMAGE_DIR='/Users/sleeping/Documents/MATLAB/ccshs_data'
+%Dataname = strcat('ccshs_1800',num2str(subj.dataset,'%03d'),'_3XG/') % -1EEG or 3EXG
