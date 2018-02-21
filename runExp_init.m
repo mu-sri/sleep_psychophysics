59e3c3e7fca5f6cc9758b8bf4448361fc400106c
Zhao Koh <zhao.koh@gmail.com>
2018-02-21 17:12:01 +1100

    Resolve conflicts in runExp_init.m and added mouse release detection in twoScreens.m

--- runExp_init.m
+++ runExp_init.m
@@ -1,6 +1,3 @@
 PARTICIPANT_RESULT_SAVE_DIR='/Users/sleeping/Documents/MATLAB/ccshs_data/Result/'
-IMAGE_DIR='/Users/sleeping/Documents/MATLAB/ccshs_data'
-<<<<<<< HEAD
+IMAGE_DIR='/Users/Zhao/SleepPsychoPhysics/Source/sleep_data/ccshs_data/'
 %Dataname = strcat('ccshs_1800',num2str(subj.dataset,'%03d'),'_3XG/') % -1EEG or 3EXG
-=======
->>>>>>> b0c2e8a6ab36d8bd49dba2b75107b33e0b3c6668
