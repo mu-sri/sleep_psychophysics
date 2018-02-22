3f53e0de1d723b65052b5137b9e66b1cac8c9933
lab user <zhao.koh@gmail.com>
2018-02-22 16:56:52 +1100

    Sync up the configuration between lab and master

--- runExp_init.m
+++ runExp_init.m
@@ -1,3 +1,4 @@
-PARTICIPANT_RESULT_SAVE_DIR='/Users/sleeping/Documents/MATLAB/ccshs_data/Result/'
-IMAGE_DIR='/Users/Zhao/SleepPsychoPhysics/Source/sleep_data/ccshs_data/'
+PARTICIPANT_RESULT_SAVE_DIR='/Users/sleeping/Downloads/sleep_psychophysics-master/Results'
+IMAGE_DIR='/Users/sleeping/Downloads/sleep_psych_data/'
 %Dataname = strcat('ccshs_1800',num2str(subj.dataset,'%03d'),'_3XG/') % -1EEG or 3EXG
+
